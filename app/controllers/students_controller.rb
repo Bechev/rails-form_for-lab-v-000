8ca63cd2de337d276e768f7cedb43928b82650b0
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-06-09 16:17:46 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,14 @@
 class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
   def create
     @student = Student.new(params.require(:student))
     @student.save
     redirect_to student_path(@student)
   end
+
+  
 end
