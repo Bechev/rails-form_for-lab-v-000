4291cf9d9d7d904537620c5767db6a08688b6eae
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-06-09 15:43:52 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,16 +1,17 @@
 class SchoolClassesController < ApplicationController
-  
+
 
   def index
   end
 
   def show
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def new
     @school_class = SchoolClass.new
   end
-  
+
   def create
     @school_class = SchoolClass.new(params.require(:school_class))
     @school_class.save
