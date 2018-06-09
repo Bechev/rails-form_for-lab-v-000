10709a4ecd64224eaf80115d8c131cabb23711d6
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-06-09 15:18:31 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,6 +5,6 @@ class SchoolClassesController < ApplicationController
     @schoolclass.title = params[:title]
     @schoolclass.room_number = params[:room_numbers]
     @schoolclass.save
-    redirect_to schoolclass_path(@schoolclass)
+    redirect_to school_classes_path(@schoolclass)
   end
 end
