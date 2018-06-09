b1a4888ab05d6925e4a4eda0e689a4174a5b5cf4
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-06-09 15:03:08 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,6 +3,6 @@ class SchoolClassesController < ApplicationController
   def create
     @schoolclass = SchoolClass.new
     @schoolclass.title = params[:title]
-    @schoolclass.room_numbers = params[:room_numbers]
+    @schoolclass.room_number = params[:room_numbers]
   end
 end
